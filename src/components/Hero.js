450bfc134ce8a8e5ab18f097c4f9551abb2998d2
Christopher Villarin <christopher.villarin@hogarth.com>
2024-04-08 09:02:33 +0800

    no message

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,13 +6,13 @@ const Hero = () => {
         // bg-[url('https://fastly.picsum.photos/id/972/1600/900.jpg?grayscale&hmac=W638WD8WqRiozMXbLvTcudGIQ8EFsXe7ljBjSXFSCz0')] bg-center
         //  style={{ backgroundImage: `url(${bgImage})` }}
         <div className="w-full flex flex-col md:flex-row text-[#030303] font-light ">
-            <div className="w-full h-[70vh] flex flex-col text-left justify-center mx-auto sm:max-w-[640px] md:max-w-[768px] lg:max-w-[924px] xl:max-w-[1536px]"> {/**/}
-                <p className='text-4xl p-4 font-light w-full leading-snug'>Hey there, I'm<br />  <p className='text-6xl md:text-8xl lg:text-9xl xl:text-9xl font-medium bebas-neue-regular '>CHRISTOPHER<br/>VILLARIN</p></p>
+            <div className="w-full h-[70vh] flex flex-col text-left justify-center mx-auto sm:max-w-[640px] md:max-w-[768px] lg:max-w-[924px] xl:max-w-[1240px]"> {/**/}
+                <p className='text-4xl p-4 font-light w-full leading-snug'>Hey there, I'm<br />  <p className='text-6xl md:text-8xl lg:text-9xl xl:text-9xl font-medium bebas-neue-regular '>CHRISTOPHER<br />VILLARIN</p></p>
                 <p className='text-md  text-[#626c81] w-full font-regular '>a Front End Developer / Creative Developer</p>
-                <p className='text-md pb-4 text-[#626c81] w-full font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br/>tempor incididunt ut labore et dolore magna aliqua. </p>
+                <p className='text-md pb-4 text-[#626c81] w-full font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br />tempor incididunt ut labore et dolore magna aliqua. </p>
                 <div className='w-full flex flex-row'>
-                    <button className='bg-[#fac541] text-[#000] w-[180px] rounded-full font-medium  py-3 px-1 mr-3 '>About me</button>
-                    <button className='bg-[#000] text-[#fff] w-[180px] rounded-full font-medium  py-3 px-1 '>Contact</button>
+                    <button className='bg-[#fac541] hover:bg-[#f3f3f3] text-[#000] hover:text-[#000] w-[180px] rounded-full font-medium  py-3 px-1 mr-3 '>About me</button>
+                    <button className='bg-[#000] hover:bg-[#f3f3f3] text-[#fff] hover:text-[#000] w-[180px] rounded-full font-medium  py-3 px-1 '>Contact</button>
                     {/* <button className='bg-[#4267B2] text-[#000] w-[50px] rounded-full font-medium  py-3 px-4 ml-4'></button>
                     <button className='bg-[#0077B5] text-[#000] w-[50px] rounded-full font-medium  py-3 px-4 ml-4'></button> */}
                 </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,21 +5,23 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <div className='w-full  text-[#000000] font-light py-7 sm:py-7  md:py-7 lg:py-8 xl:py-8'>
-            <div className="h-16  mx-auto md:px-14 lg:px-17 flex "> {/** max-w-[1240px]*/}
+        <div className='w-full  text-[#000000] font-light py-7 sm:py-7  md:py-7 lg:py-8 xl:py-8 '>
+            <div className="h-16  mx-auto md:px-14 lg:px-17 flex"> {/** max-w-[1240px]*/}
                 {/* <div className="p-8 w-full bg-slate-300"> </div> */}
-                <div className='w-full flex flex-row justify-between items-center'>                
+                <div className='w-full flex flex-row justify-between items-center'>
                     <div className='flex text-1xl md:text-2xl lg:text-3xl font-bold mr-5 text-[#fac541]'>VILL<span className=' text-black '>ARIN</span></div>
-                    <ul className='flex bg-[#f3f3f3] rounded-full font-medium text-sm sm:text-sm md:text-sm lg:text-sm xl:text-xl'>
+                    <ul className='flex bg-[#f3f3f3] rounded-full font-medium 
+                    text-sm sm:text-sm md:text-sm lg:text-sm xl:text-md 
+                    hidden sm:flex md:flex lg:flex xl:flex'>
                         <li className='p-4 mx-5'>HOME</li>
                         <li className='p-4 mx-5'>ABOUT</li>
                         <li className='p-4 mx-5'>CONTACT</li>
                         {/* <button className='bg-[#000000] text-[#FFF] w-[100px] h-5/6 rounded-full font-regular'>Learn More</button> */}
                     </ul>
-                    <ul className='flex bg-[#f3f3f3] rounded-full font-medium text-sm sm:text-sm md:text-sm lg:text-sm xl:text-xl'>
+                    <ul className='flex bg-[#f3f3f3] rounded-full font-medium text-sm sm:text-sm md:text-sm lg:text-sm xl:text-md'>
                         <li className='p-4 mx-5'>CART</li>
-                    
-                       
+
+
                     </ul>
                 </div>
 
