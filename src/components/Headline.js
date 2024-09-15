2f73cb3a7326310f64898918194f23146ad8d576
christopher-villarin <christopher.villarin@hogarth.com>
2024-09-15 20:16:39 +0800

    update cta

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -50,8 +50,8 @@ function Headline() {
                             
                                 <p className='text-sm px-8 sm:px-8 md:px-4 text-[#626c81] w-full font-light transition-all duration-300 ease-in-out'>a Front End Developer / Creative Developer<br></br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br />tempor incididunt ut labore et dolore magna aliqua. </p>
                                 <div className='w-full flex flex-row px-8 sm:px-8 md:px-4 '>
-                                <button className='text-sm bg-[#508989] hover:bg-[#f3f3f3] text-[#fff] hover:text-[#2E2F2F] w-[140px] rounded-full font-medium  py-3 px-1 mr-3 transition-all duration-300 ease-in-out' style={{ transition: 'background-color 0.3s' }}>About me</button>
-                                <button className='text-sm bg-[#f3f3f3] hover:bg-[#2E2F2F] text-[#1B224A] hover:text-[#fff] w-[140px] rounded-full font-medium  py-3 px-1  transition-all duration-300 ease-in-out ' style={{ transition: 'background-color 0.3s' }}>Contact</button>
+                                <button className='text-sm bg-[#2E2F2F] hover:bg-[#f3f3f3] text-[#fff] hover:text-[#2E2F2F] w-[140px] rounded-full font-medium  py-3 px-1 mr-3 transition-all duration-300 ease-in-out' style={{ transition: 'background-color 0.3s' }}>Contact me</button>
+                                {/* <button className='text-sm bg-[#f3f3f3] hover:bg-[#2E2F2F] text-[#1B224A] hover:text-[#fff] w-[140px] rounded-full font-medium  py-3 px-1  transition-all duration-300 ease-in-out ' style={{ transition: 'background-color 0.3s' }}>Contact</button> */}
                                 
                                 {/* <Link to="https://www.linkedin.com/in/christophervillarin" target="_blank" rel="noopener noreferrer" className='pt-3 pb-3  pl-3 pr-3 bg-[#fff] rounded-l-full'>
                                         <IconLinkedin></IconLinkedin>
